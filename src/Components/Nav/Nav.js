f98181162a52710bc8e80bbc56eb7ad73d9175a7
Nishat009 <nishatraihana009@gmail.com>
2021-08-21 14:58:19 +0600

    final

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -4,22 +4,18 @@ import { Link } from 'react-router-dom';
 import "./Nav.css";
 const Nav = () => {
   return (
-    <div className="container-fluid">
-      <h1 className="text-center">Assessment Project</h1>
-      <div class="nav-background ">
-        <Navbar expand="lg">
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse className=" ">
-            <Link to="/registration" className="nav-menu">
-              USER REGISTRATION
-            </Link>
-            <Link to="/post" className="nav-menu">
-              POSTS
-            </Link>
-          </Navbar.Collapse>
-        </Navbar>
-      </div>
-    </div>
+    <section className="container-fluid">
+            <h1 className="text-center">Assesment Project</h1>
+            <div class="nav-background ">
+                <Navbar expand="lg">
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse className=" ">
+                        <Link to="/registration" className="nav-link">USER REGISTRATION</Link>
+                        <Link to="/post" className="nav-link">POSTS</Link>
+                    </Navbar.Collapse>
+                </Navbar>
+            </div>
+        </section>
   );
 };
 
